7965d8abd69b6606fee98c19ecd041901115319c
ajayadsule <ajayadsule5@gmail.com>
2026-06-02 16:22:44 +0530

    feat: configure axios and tanstack query

---
+++ src/services/httpRequest.ts
@@ -0,0 +1,50 @@
+import axios, { type AxiosResponse } from "axios";
+
+export const httpMethods = {
+  get: "GET",
+  post: "POST",
+  patch: "PATCH",
+  put: "PUT",
+  delete: "DELETE",
+} as const;
+
+export type HttpMethod = (typeof httpMethods)[keyof typeof httpMethods];
+
+interface HttpRequestProps<T = unknown, U = unknown> {
+  url: string;
+  method: HttpMethod;
+  options?: T;
+  payload?: U;
+  params?: Record<string, unknown>;
+}
+
+const api = axios.create({
+  baseURL: "https://fakestoreapi.com",
+  headers: {
+    Accept: "application/json, text/plain",
+  },
+});
+
+const httpRequest = async <R, T = unknown, U = unknown>({
+  url = "",
+  method = httpMethods.get,
+  options,
+  payload,
+  params,
+}: HttpRequestProps<T, U>): Promise<R> => {
+  return await api({
+    ...(options as object),
+    url,
+    method,
+    data: payload,
+    params,
+  })
+    .then((response: AxiosResponse<R>) => {
+      return response.data;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+export { httpRequest };
