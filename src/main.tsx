a58fb38d5b757876dc536daf10abe0f4c06daff8
ajayadsule <ajayadsule5@gmail.com>
2026-06-03 12:45:12 +0530

    feat: Add tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import './styles/global.scss';
+import { router } from './router.tsx';
+import { RouterProvider } from '@tanstack/react-router';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -15,7 +15,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
